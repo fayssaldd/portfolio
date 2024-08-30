99f75611c2bf4a020a57f53f5c4edf61a91d476a
fayssal dd <fayssaldd982@gmail.com>
2024-08-30 12:08:55 +0100

    feat: react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Contact from './pages/Contact.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path:"/testDeploy/",
+    element:<App/>,
+    children:[
+      {
+        path:"/testDeploy/",
+        element:<Home/>
+      },
+      {
+        path:"/testDeploy/contact",
+        element:<Contact/>
+      }
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
