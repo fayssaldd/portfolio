db000f675648a91ff20970c21c7fd46c1f920ef4
fayssal dd <fayssaldd982@gmail.com>
2024-08-31 20:33:10 +0100

    modifer navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ModeToggle } from './components/mode-toggle';
 
 export default function NavBar() {
@@ -6,6 +6,8 @@ export default function NavBar() {
   console.log('====================================');
   // console.log(par);
   console.log('====================================');
+  const [isShow,setIsShow] = useState(false);
+
   return (
     <div>
         <nav className="backdrop-blur-sm fixed w-full shadow">
@@ -13,7 +15,7 @@ export default function NavBar() {
               <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* <!-- Mobile menu button--> */}
-                  <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                  <button onClick={()=>setIsShow(!isShow)} type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                     <span className="absolute -inset-0.5"></span>
                     <span className="sr-only">Open main menu</span>
                     <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -44,7 +46,7 @@ export default function NavBar() {
             
           </div>
           </div>       
-        <div className="sm:hidden" id="mobile-menu">
+        <div className={`${!isShow && "hidden"}`} id="mobile-menu">
           <div className="space-y-1 px-2 pb-3 pt-2">
             {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
             <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
