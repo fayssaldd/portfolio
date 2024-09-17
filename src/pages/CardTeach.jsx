028852a5aebe1117b893a457321f32156731c7a0
fayssal dd <fayssaldd982@gmail.com>
2024-09-17 18:18:26 +0100

    add swiper to techonologie

--- src/pages/CardTeach.jsx
+++ src/pages/CardTeach.jsx
@@ -10,20 +10,20 @@ import {
 export default function CardTeach({titre,description,image}) {
   return (
     <>
-    <div className='w-52 m-3'>
+    {/* <div className=''> */}
         <Card className="shadow dark:text-white text-black " >
             <CardHeader>
                 <CardTitle>{titre}</CardTitle>
                 <CardDescription>{description}</CardDescription>
             </CardHeader>
-            <CardContent>
+            <CardContent className="flex aspect-square items-center justify-center p-6">
                 <img className='h-40' src={`${image}`} alt="" />
             </CardContent>
             <CardFooter>
                 <p>Card Footer</p>
             </CardFooter>
         </Card>
-    </div>
+    {/* </div> */}
     </>
   )
 }
--- src/pages/Teachnologie.jsx
+++ src/pages/Teachnologie.jsx
@@ -10,16 +10,70 @@ import mySql from '../assets/teachnologie/mySql.png'
 import react from '../assets/teachnologie/react.png'
 import shadcn from '../assets/teachnologie/shadcn.png'
 import laravel from '../assets/teachnologie/laravel.png'
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+ 
+import { Card, CardContent } from "@/components/ui/card"
+import { m } from 'framer-motion'
 export default function Teachnologie() {
   const lien = "../assets/teachnologie/"
-
+  // const images =  [html, css, bootstrap, tailwind, javascript, php, mySql, react, shadcn, laravel]
+  const technologies = {
+    html: {
+      nom: html,
+      description: "HyperText Markup Language, utilisé pour structurer le contenu des pages web."
+    },
+    css: {
+      nom: css,
+      description: "Cascading Style Sheets, utilisé pour styliser et mettre en forme les pages web."
+    },
+    bootstrap: {
+      nom: bootstrap,
+      description: "Un framework CSS qui facilite la création de sites web réactifs et modernes."
+    },
+    tailwind: {
+      nom: tailwind,
+      description: "Un framework CSS utilitaire qui permet de créer rapidement des interfaces stylées avec des classes pré-définies."
+    },
+    javascript: {
+      nom: javascript,
+      description: "Un langage de programmation pour rendre les pages web interactives et dynamiques."
+    },
+    php: {
+      nom: php,
+      description: "Un langage de script côté serveur utilisé pour développer des applications web dynamiques."
+    },
+    mySql: {
+      nom: mySql,
+      description: "Un système de gestion de base de données relationnel open-source utilisé pour stocker et gérer des données."
+    },
+    react: {
+      nom: react,
+      description: "Une bibliothèque JavaScript pour la création d'interfaces utilisateur interactives, développée par Facebook."
+    },
+    shadcn: {
+      nom: shadcn,
+      description: "Une collection de composants UI prête à l'emploi pour React, souvent utilisée avec Tailwind CSS."
+    },
+    laravel: {
+      nom: laravel,
+      description: "Un framework PHP robuste qui simplifie le développement d'applications web avec une syntaxe élégante."
+    }
+  };
+  
+  console.log(technologies);
+  
   return (
-    <div id='technologie' className=''>
+    <div id='technologie' className='flex flex-col justify-center items-center'>
         <div>
-            <p className=' text-center text-3xl underline'>Teachnologies And Framworks</p>
+            <p className=' text-center text-3xl underline mb-4'>Teachnologies And Framworks</p>
         </div>
-        <div className='flex flex-wrap justify-center w-full '>
+        {/* <div className='flex flex-wrap justify-center w-full '>
             <CardTeach titre={"html5"} description={"description"} image={html} />
             <CardTeach titre={"css3"} description={"description"} image={css} />
             <CardTeach titre={"bootstrap"} description={"description"} image={bootstrap} />
@@ -30,7 +84,30 @@ export default function Teachnologie() {
             <CardTeach titre={"react"} description={"description"} image={react} />
             <CardTeach titre={"shadcn"} description={"description"} image={shadcn} />
             <CardTeach titre={"laravel"} description={"description"} image={laravel} />
-        </div>
+        </div> */}
+        <Carousel className="w-full max-w-4xl">
+          <CarouselContent className="-ml-1">
+            {
+              Object.entries(technologies).map(([key, value]) => (
+                <CarouselItem  className="pl-1 md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                    <CardTeach titre={key} description={value.description} image={value.nom} />
+                </div>
+                </CarouselItem>
+              ))
+            }
+            {/* {images.map((im, index) => (
+              <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                <CardTeach titre={"laravel"} description={"description"} image={im} />
+                  
+                </div>
+              </CarouselItem>
+            ))} */}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
     </div>
   )
 }
