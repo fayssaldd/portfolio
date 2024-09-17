922bd979994b3155a0e5717e6466ec2c6da5e117
fayssal dd <fayssaldd982@gmail.com>
2024-09-17 17:11:17 +0100

    modifer router

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
   },
   {
     path:"/",
-    element:<h1>Hi</h1>
+    element: <Navigate to={"/portfolio/"}/>
   }
 ])
 createRoot(document.getElementById('root')).render(
