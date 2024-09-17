c4389514acd803f7925f1f315399c69bd4b0a153
fayssal dd <fayssaldd982@gmail.com>
2024-09-17 16:34:55 +0100

    bas to vitejs

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Contact from './pages/Contact.jsx'
 
@@ -19,6 +19,10 @@ const router = createBrowserRouter([
       {
         path:"/portfolio/contact",
         element:<Contact/>
+      },
+      {
+        path:"/",
+        element:<Navigate to={'portfolio'}/>
       }
     ]
   }
