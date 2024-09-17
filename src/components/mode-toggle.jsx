c8f5f76ac46814dda2395f185f986050cd72eac3
fayssal dd <fayssaldd982@gmail.com>
2024-09-17 18:26:47 +0100

    modifer to togle dackmode

--- src/components/mode-toggle.jsx
+++ src/components/mode-toggle.jsx
@@ -4,14 +4,14 @@ import {useTheme} from "@/components/theme-provider"
 
 export function ModeToggle() {
   const {setTheme, theme} = useTheme()
-  console.log(theme,'hhh');
   
   return (
     <Button variant="ghost" onClick={setTheme} size="icon">
-      {theme == 'light' ?
+      {theme === 'light' ?
         <Sun className="h-[1.2rem] w-[1.2rem] text-black rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
         :
-        <Moon className="h-[1.2rem] w-[1.2rem] text-white rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+
 
       }
       <span className="sr-only">Toggle theme</span>
