ec30746cbf9af5c9b612384c93dc8638c63d4dd6
fayssal dd <fayssaldd982@gmail.com>
2024-10-02 19:29:49 +0100

    ajouter schroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { useEffect, useState } from 'react'
 
 import './App.css'
 import NavBar from './NavBar'
-import { motion } from "framer-motion"
+import { motion, useScroll, useSpring } from "framer-motion"
 import { ThemeProvider } from './components/theme-provider'
 import {useTheme} from "@/components/theme-provider"
 import Main from './pages/Main'
-import { useCallback } from "react";
-import Particles from "react-particles";
-import { loadSlim } from "tsparticles-slim"
-import particlesConfig from './components/config/particles-config'
+
 function App() {
-  const {theme} = useTheme()
-  console.log(theme + 'hhh');
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
   
 
   const [isShow,setIsShow] = useState(true)
@@ -25,7 +26,6 @@ function App() {
   return (
     <>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-   
       {
         isShow ? <div className='bg-black w-full h-screen text-green-500 flex items-center justify-center text-7xl'>
           <motion.p
@@ -37,7 +37,10 @@ function App() {
           </motion.p>
         </div>
         :<>
+        <motion.div className="fixed top-0 left-0 right-0 h-[4px] bg-green-500 z-50 origin-[0%]" style={{ scaleX }} />
+          
         <div>
+
           <NavBar/>
         </div>
         <div>
@@ -45,7 +48,7 @@ function App() {
         </div>
         </>
       }
-
+  
        </ThemeProvider>
 
 
